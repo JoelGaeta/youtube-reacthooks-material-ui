4bed409c2c039b969a7811a667ee328ef309d06d
Joel Gaeta <joelsgaeta@gmail.com>
2020-02-10 15:54:50 -0500

    Updates

---
+++ precache-manifest.7ef4c309ac58fb108e1f71e67597213a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387807b4f27c568c88f8266790981a68",
+    "url": "/youtube-reacthooks-material-ui/index.html"
+  },
+  {
+    "revision": "da46b13c76860abc8a02",
+    "url": "/youtube-reacthooks-material-ui/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "136df645e628c209dc99",
+    "url": "/youtube-reacthooks-material-ui/static/js/2.476e7432.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/youtube-reacthooks-material-ui/static/js/2.476e7432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da46b13c76860abc8a02",
+    "url": "/youtube-reacthooks-material-ui/static/js/main.fba01a57.chunk.js"
+  },
+  {
+    "revision": "4528ab64a6704884aa4c",
+    "url": "/youtube-reacthooks-material-ui/static/js/runtime-main.40135dbe.js"
+  }
+]);
